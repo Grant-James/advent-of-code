4f6c0d95836d7d18a0e241db7758a7dfa9e2ffa8
HappySaila <granthaldanewilson836@gmail.com>
2020-11-08 16:01:53 +0200

    small change

--- 2019/day-1/main.go
+++ 2019/day-1/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	err := run()
@@ -11,5 +14,44 @@ func main() {
 }
 
 func run() error {
+	data, err := ReadFile("input.txt", ReadModeMultiline)
+	if err != nil {
+	    return err
+	}
+
+	var total int
+	for _, datum := range data {
+		n, err := strconv.Atoi(datum)
+		if err != nil {
+		    return err
+		}
+
+		total += fuelNeeded(n)
+	}
+	
+	fmt.Printf("Part1: %v\n", total)
+
+	var total2 int
+	for _, datum := range data {
+		mass, err := strconv.Atoi(datum)
+		if err != nil {
+			return err
+		}
+
+		for mass > 0 {
+			fuel := fuelNeeded(mass)
+			mass = fuel
+			if mass > 0 {
+				total2 += mass
+			}
+		}
+	}
+
+	fmt.Printf("Part2: %v\n", total2)
+
 	return nil
+}
+
+func fuelNeeded(mass int) int {
+	return mass/3-2
 }
